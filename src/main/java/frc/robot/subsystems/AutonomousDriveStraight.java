6ae230a9e65ebe4b63d15e818a542069c6c67cf6
team5016 <huntingtonrobotics@gmail.com>
2024-02-29 16:22:18 -0500

    New class created for Autonomous Drive.

---
+++ src/main/java/frc/robot/subsystems/AutonomousDriveStraight.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.Constants.DrivetrainConstants;
+
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants.CommandSwerveDrivetrain;
+
+public class AutonomousDriveStraight {
+    
+
+}
