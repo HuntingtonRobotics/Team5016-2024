ec75c6ca943b2793f1a01d9dc9457e69f2cc9311
team5016 <huntingtonrobotics@gmail.com>
2024-03-03 08:32:10 -0500

    Reduce max speed

--- src/main/java/frc/robot/SwerveDriveContainer.java
+++ src/main/java/frc/robot/SwerveDriveContainer.java
@@ -11,7 +11,7 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.TunerConstants;
 
 public class SwerveDriveContainer {
-    public double MaxSpeed = 6; // 6 meters per second desired top speed
+    public double MaxSpeed = 3; // meters per second desired top speed
     public double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
