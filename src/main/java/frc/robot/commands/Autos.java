ea99b5814c9939339761a65677e576dbd46cbf12
Jesse Abrahams <31041350+jabberhams@users.noreply.github.com>
2024-03-20 03:42:39 -0700

    Experimental driveForwardAndRotate auto method

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -14,11 +10,6 @@ import frc.robot.subsystems.Launcher;
 
 public final class Autos {
 
-  /* Original example from template; note the command structure */
-  // return new RunCommand(() -> drivetrain.arcadeDrive(-.5, 0))
-  // .withTimeout(1)
-  // .andThen(new RunCommand(() -> drivetrain.arcadeDrive(0, 0)));
-
   public static Command driveForward(SwerveDriveContainer swerve, AutonomousArgs args) {
     Command driveSequence = Commands.print("Starting driveForward sequence")
       .andThen(swerve.drivetrain.applyRequest(() -> swerve.drive.withVelocityY(swerve.MaxSpeed*0.25)))
@@ -29,6 +20,31 @@ public final class Autos {
       .andThen(Commands.print("driveForward sequence completed normally"));
       
     return driveSequence.handleInterrupt(() -> write("driveForward sequence interrupted"));
+  }
+
+  public static Command driveForwardAndRotate(SwerveDriveContainer swerve, AutonomousArgs args) {
+    /* *** These commands need review! ***
+    *  When starting from the side of a speaker, the robot may need to:
+    *   - drive and turn almost simultaneously, to get out of its zone more quickly
+    *   - drive more and turn later, to give other robots more room to move
+    * 
+    *  Do not use in competition without testing or review first
+    */
+    // Rotation direction will need to change based on the side of the speaker the robot starts from
+    //  Default is counterclockwise (negative)
+    double rotationDirection = -1;
+    Command driveSequence = Commands.print("Starting driveForwardAndRotate sequence")
+      .andThen(swerve.drivetrain.applyRequest(() ->
+         swerve.drive.withVelocityY(swerve.MaxSpeed*0.25)
+                     .withRotationalRate(rotationDirection * swerve.MaxAngularRate * 0.25)))
+      .andThen(Commands.waitSeconds(args.DriveDurationSeconds))
+      .andThen(swerve.drivetrain.applyRequest(() ->
+         swerve.drive.withVelocityY(0)
+                     .withRotationalRate(0))
+      )
+      .andThen(Commands.print("driveForward sequence completed normally"));
+      
+    return driveSequence.handleInterrupt(() -> write("driveForwardAndRotate sequence interrupted"));
   }
 
   public static Command launchToSpeaker(Intake intakeSub, Launcher launcherSub, AutonomousArgs args) {
@@ -61,7 +77,7 @@ public final class Autos {
     
   public static Command launchThenDriveForward(SwerveDriveContainer swerve, AutonomousArgs args, Intake intake, Launcher launcher) {
     return launchToSpeaker(intake, launcher, args)
-      .andThen(driveForward(swerve, args).withTimeout(2.5));
+      .andThen(driveForward(swerve, args).withTimeout(args.DriveDurationTimeoutSeconds));
       //.andThen(intakeAbit(intake, args))
       //.withTimeout(4.0); // No matter what, finish the sequence after X seconds
   }
@@ -69,6 +85,11 @@ public final class Autos {
   private static void write(String x) {
     System.out.println(x);
   }
+
+  /* Original example from template; note the command structure */
+  // return new RunCommand(() -> drivetrain.arcadeDrive(-.5, 0))
+  // .withTimeout(1)
+  // .andThen(new RunCommand(() -> drivetrain.arcadeDrive(0, 0)));
 
   public static Command launchToSpeaker_Test() {
     Command launchSequence = Commands.runOnce(() -> write("Started launcher motor"))
